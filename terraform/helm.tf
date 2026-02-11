05a613f9f5309b0da75f9f9a7f0f45af3861919c
Kirill Parasotchenko <kir7ooha@gmail.com>
2026-02-11 12:43:50 +0100

    service

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,3 +1,18 @@
+resource "helm_release" "ingress_nginx" {
+  name = "ingress-nginx"
+  namespace = "ingress-nginx"
+  create_namespace = true
+
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart = "ingress-nginx"
+  version = "4.12.3"
+
+  set {
+    name = "controller.service.type"
+    value = "LoadBalancer"
+  }
+}
+
 resource "helm_release" "kuber_practice_app" {
   name = "kuber-practice-app"
   chart = "../chart"
